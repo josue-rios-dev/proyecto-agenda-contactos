7141936992e7dc6192de39723336ca8d83c3d6b7
Josue Rios Gonzales <325831364+josue-rios-dev@users.noreply.github.com>
2026-09-19 21:08:15 -0500

    Configura el proyecto con Vite, React y Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        whatsapp: "#25D366"
+      }
+    }
+  },
+  plugins: []
+}
